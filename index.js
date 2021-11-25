ab3c6d18654dfbd8689ed4c44168a46f0c2ee09a
DESKTOP-L7JCQ4N\silk <531024850@qq.com>
2021-11-25 16:29:27 +0800

    new test

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function zhu(){
   console.log('朱傲写的代码');
-  console.long('周文龙改的！！！');
+}
+function zhou(){
+  console.log('周文龙');
+  console.long('周文龙改的！');
+}
+function zhu(){
+  console.log('此函数原名为zhu');
 }
